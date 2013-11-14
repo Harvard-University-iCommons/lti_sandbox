db260d6e85de63781754b7b8eda2ec92a49d62d7
Colin Murtaugh <colin_murtaugh@harvard.edu>
2013-11-14 15:11:26 -0500

    added oracle client installation, pulling the zip archives from test.isites.harvard.edu

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -109,3 +109,79 @@ file {'/etc/profile.d/venvwrapper.sh':
     require => Package['virtualenvwrapper'],
 }
 
+# install the Oracle instant client
+
+define download ($uri, $timeout = 300) {
+  exec {
+      "download $uri":
+          command => "wget -q '$uri' -O $name",
+          creates => $name,
+          timeout => $timeout,
+          require => Package[ "wget" ],
+  }
+}
+
+download {
+  "/tmp/instantclient-basiclite-linux.x64-11.2.0.3.0.zip":
+      uri => "http://test.isites.harvard.edu/oracle/instantclient-basiclite-linux.x64-11.2.0.3.0.zip",
+      timeout => 900;
+}
+
+download {
+  "/tmp/instantclient-sqlplus-linux.x64-11.2.0.3.0.zip":
+      uri => "http://test.isites.harvard.edu/oracle/instantclient-sqlplus-linux.x64-11.2.0.3.0.zip",
+      timeout => 900;
+}
+
+download {
+  "/tmp/instantclient-sdk-linux.x64-11.2.0.3.0.zip":
+      uri => "http://test.isites.harvard.edu/oracle/instantclient-sdk-linux.x64-11.2.0.3.0.zip",
+      timeout => 900;
+}
+
+file {'/opt/oracle':
+    ensure => directory,
+}
+
+exec {'instantclient-basiclite':
+    require => [ Download['/tmp/instantclient-basiclite-linux.x64-11.2.0.3.0.zip'], File['/opt/oracle'], Package['unzip'] ],
+    cwd => '/opt/oracle',
+    command => 'unzip /vagrant/vagrant/files/instantclient-basiclite-linux.x64-11.2.0.3.0.zip',
+    creates => '/opt/oracle/instantclient_11_2/BASIC_LITE_README',
+}
+
+exec {'instantclient-sqlplus':
+    require => [ Download['/tmp/instantclient-sqlplus-linux.x64-11.2.0.3.0.zip'], File['/opt/oracle'], Package['unzip'] ],
+    cwd => '/opt/oracle',
+    command => 'unzip /vagrant/vagrant/files/instantclient-sqlplus-linux.x64-11.2.0.3.0.zip',
+    creates => '/opt/oracle/instantclient_11_2/sqlplus',
+}
+
+exec {'instantclient-sdk':
+    require => [ Download['/tmp/instantclient-sdk-linux.x64-11.2.0.3.0.zip'], File['/opt/oracle'], Package['unzip'] ],
+    cwd => '/opt/oracle',
+    command => 'unzip /vagrant/vagrant/files/instantclient-sdk-linux.x64-11.2.0.3.0.zip',
+    creates => '/opt/oracle/instantclient_11_2/sdk',
+}
+
+file {'/opt/oracle/instantclient_11_2/libclntsh.so':
+    ensure => link,
+    target => 'libclntsh.so.11.1',
+    require => Exec['instantclient-basiclite'],
+}
+
+file {'/opt/oracle/instantclient_11_2/libocci.so':
+    ensure => link,
+    target => 'libocci.so.11.1',
+    require => Exec['instantclient-basiclite'],
+}
+
+file {'/etc/profile.d/oracle.sh':
+    ensure => file,
+    content => 'export ORACLE_HOME=/opt/oracle/instantclient_11_2; export PATH=$ORACLE_HOME:$PATH; export LD_LIBRARY_PATH=$ORACLE_HOME',
+    mode => '755',
+    require => Exec['instantclient-basiclite'],
+}
+
+
+
