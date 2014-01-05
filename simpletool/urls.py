22dd861e10606fe623ae3ac712ef95898d054f77
Colin Murtaugh <cmurtaugh@gmail.com>
2014-01-05 12:03:27 -0500

    First pass at adding index and tool_config pages which do not require authentication.

--- simpletool/urls.py
+++ simpletool/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, url
 
 
-
 urlpatterns = patterns('',
 
-    url(r'^$', 'simpletool.views.index'),
+    url(r'^$', 'simpletool.views.index', name='index'),
 
-    url(r'^lti_tool$', 'simpletool.views.lti_tool'),
+    url(r'^lti_tool$', 'simpletool.views.lti_tool', name='lti_tool'),
+
+    url(r'^tool_config$', 'simpletool.views.tool_config', name='tool_config'),
 
     url(r'^another_page$', 'simpletool.views.another_page'),
-)
 
+)
--- simpletool/views.py
+++ simpletool/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-from ims_lti_py.tool_provider import DjangoToolProvider
+from ims_lti_py.tool_config import ToolConfig
 
-from time import time
 import logging
 
 logger = logging.getLogger(__name__)
@@ -17,37 +19,33 @@ def index(request):
     return render(request, 'simpletool/index.html')
 
 
-@require_http_methods(['GET','POST'])
+@login_required
+@require_http_methods(['POST'])
 def lti_tool(request):
 
     if request.user.is_authenticated:
         return render(request, 'simpletool/lti_tool.html', {'request': request})
 
-    else: 
+    else:
         return render(request, 'simpletool/error.html', {'message': 'Error: user is not authenticated!'})
+
 
 def another_page(request):
     return render(request, 'simpletool/another_page.html')
 
-    
-'''
+
 @require_http_methods(['GET'])
 def tool_config(request):
-    host = request.scheme + '://' + request.host
-    secure_host = 'https://' + request.host
-    url = host + '/lti_tool'
-    secure_url = secure_host + '/lti_tool'
+    host = 'http://' + request.get_host()
+    secure_host = 'https://' + request.get_host()
+    url = host + reverse('st:lti_tool')
+    secure_url = secure_host + reverse('st:lti_tool')
+
     lti_tool_config = ToolConfig(
-        title='Example Django Tool Provider',
+        title='LTI Sandbox Simple Tool',
         launch_url=url,
-        secure_launch_url=secure_url)
-    lti_tool_config.description = 'This example LTI Tool Provider supports LIS Outcome pass-back'
-    resp = response(lti_tool_config.to_xml(), 200)
-    resp.headers['Content-Type'] = 'text/xml' 
+        secure_launch_url=secure_url,
+    )
+    lti_tool_config.description = 'This is a simple LTI tool.'
+    resp = HttpResponse(lti_tool_config.to_xml(), content_type='text/xml', status=200)
     return resp
-'''
-
-
-
-
-
