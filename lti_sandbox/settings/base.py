d75c9b9404c393c3de5c34fd948326843c535889
Colin Murtaugh <cmurtaugh@gmail.com>
2014-01-06 08:07:43 -0500

    remove the LTI auth middleware - we’ll enforce authentication on a view-by-view basis instead

--- lti_sandbox/settings/base.py
+++ lti_sandbox/settings/base.py
@@ -84,7 +84,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django_auth_lti.middleware.LTIAuthMiddleware',
+    #'django_auth_lti.middleware.LTIAuthMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
